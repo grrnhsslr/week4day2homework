83c40f06c602133456beedb3538b5257c3a25e9e
grrnhsslr <hasslergarren@gmail.com>
2024-03-13 10:59:54 -0500

    alternative solution\

--- exercise4.py
+++ exercise4.py
@@ -3,18 +3,34 @@
 # More information on the Fibonacci Sequence here. Start the sequence with `0,1,1,...`.
 
 
-def fibonacci(num):
-    num1 = 0
-    num2 = 1
-    next_number = num2
-    count = 1
+def fibonacci(n):
+    # num1 = 0
+    # num2 = 1
+    # next_number = num2
+    # count = 1
+    #
+    # while count <= num:
+    #     print(next_number, end=' ')
+    #     count += 1
+    #     num1, num2 = num2, next_number
+    #     next_number = num1 + num2
+    if n < 0:
+        print("Incorrect input")
 
-    while count <= num:
-        print(next_number, end=' ')
-        count += 1
-        num1, num2 = num2, next_number
-        next_number = num1 + num2
+        # Check if n is 0
+        # then it will return 0
+    elif n == 0:
+        return 0
+
+        # Check if n is 1,2
+        # it will return 1
+    elif n == 1 or n == 2:
+        return 1
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 fib = input('pick a number: ')
+
 print(fibonacci(int(fib)))
